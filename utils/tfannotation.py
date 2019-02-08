80caa1d39f25ec65d6b7ceba10ccac018b98bf3f
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2019-02-08 16:56:58 -0700

    Finished implementing build method of TFAnnotation
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- utils/tfannotation.py
+++ utils/tfannotation.py
@@ -31,3 +31,30 @@ class TFAnnotation:
         h = int64_feature(self.height)
         filename = bytes_feature(self.filename.encode("utf8"))
         encoding = bytes_feature(self.image)
+        image = bytes_feature(self.image)
+        x_mins = float_list_feature(self.x_mins)
+        x_maxs = float_list_feature(self.x_maxs)
+        y_mins = float_list_feature(self.y_mins)
+        y_maxs = float_list_feature(self.y_maxs)
+        text_labels = bytes_list_feature(self.text_labels)
+        classes = int64_list_feature(self.classes)
+        difficult = int64_list_feature(self.difficult)
+
+        data = {
+            "image/height": h,
+            "image/width": w,
+            "image/filename": filename,
+            "image/source_id": filename,
+            "image/encoded": image,
+            "image/format": encoding,
+            "image/object/bbox/xmin": x_mins,
+            "image/object/bbox/xmax": x_maxs,
+            "image/object/bbox/ymin": y_mins,
+            "image/object/bbox/ymax": y_maxs,
+            "image/object/class/text": text_labels,
+            "image/object/class/label": classes,
+            "image/object/difficult": difficult,
+        }
+
+        # Return the data dictionary
+        return data
